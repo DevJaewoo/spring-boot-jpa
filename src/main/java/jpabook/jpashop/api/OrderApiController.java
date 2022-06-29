2ef745cc497ad4b36aac787d29d92ce9cbe5a4cc
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-06-29 21:07:30 +0900

    컬렉션 주문 조회 V2 개발 (엔티티를 DTO로 변환)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,5 +36,48 @@ public class OrderApiController {
         }
 
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDTO> readOrdersV2() {
+        return orderRepository.findAllByString(new OrderSearch()).stream()
+                .map(OrderDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class OrderDTO {
+        private Long orderID;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        private List<OrderItemDTO> orderItems;
+
+        public OrderDTO(Order order) {
+            this.orderID = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getOrderStatus();
+            this.address = order.getDelivery().getAddress();
+            this.orderItems = order.getOrderItems().stream().map(OrderItemDTO::new).collect(Collectors.toList());
+        }
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class OrderItemDTO {
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDTO(OrderItem orderItem) {
+            this.itemName = orderItem.getItem().getName();
+            this.orderPrice = orderItem.getOrderPrice();
+            this.count = orderItem.getCount();
+        }
     }
 }
